458499ca1196d9ed83a9bfc6e51b68fcee16145e
apeiris <apeiris@hotmail.com>
2020-10-16 21:07:47 -0400

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,29 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * 
+ *
  * this code is based on
  * https://github.com/WebThingsIO/webthing-arduino/blob/master/examples/LEDLamp/LEDLamp.ino
  */
 #define LARGE_JSON_BUFFERS 1
 
-#include <Arduino.h>
-#include "Thing.h"
-#include "WebThingAdapter.h"
-#include <SPI.h>
-#include "LedControl.h"
 #include <Adafruit_AHTX0.h>
-#include <analogWrite.h>
+#include <Arduino.h>
+#include <ArduinoOTA.h>
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
-#include <ArduinoOTA.h>
+#include <SPI.h>
+#include <analogWrite.h>
+
 #include <map>
-#include "password.h"
-#include "myUtils.h"
-#include "esp_system.h"
+
+#include "LedControl.h"
+#include "Thing.h"
+#include "WebThingAdapter.h"
 #include "esp_log.h"
+#include "esp_system.h"
+#include "myUtils.h"
+#include "password.h"
 
 using namespace std;
 static const char *TAG = "main.cpp";
@@ -39,29 +41,31 @@ static const char *TAG = "main.cpp";
 IPAddress mySqlIP(192, 168, 0, 10);
 /// Only used for monitoring, can be removed it's not part of our "thing"
 
-#define pushInterval (60000 / 100)
+#define pushInterval (60000 / 1)
 #if defined(LED_BUILTIN)
 const int ledPin = LED_BUILTIN;
 #else
-const int ledPin = 2; // manually configure LED pin was 13 2 is blue led built in
+const int ledPin =
+    2;  // manually configure LED pin was 13 2 is blue led built in
 const int lampPin = 2;
 #endif
 // AHT10 pins
-const int pinSDA = 5;  // white wire
-const int pinSCL = 17; // the purple
+const int pinSDA = 5;   // white wire
+const int pinSCL = 17;  // the purple
 
 // pin definitions for MAX7129
-const int pinCS = 15;  // Chip Select
-const int pinCLK = 18; // Clock pin
-const int pinDIN = 23; // Data
-const int anzMAX = 1;  //Anzahl der kaskadierten  Module = Number of Cascaded modules
+const int pinCS = 15;   // Chip Select
+const int pinCLK = 18;  // Clock pin
+const int pinDIN = 23;  // Data
+const int anzMAX =
+    1;  // Anzahl der kaskadierten  Module = Number of Cascaded modules
 
-long mmap(long x, long in_min, long in_max, long out_min, long out_max)
-{
+long mmap(long x, long in_min, long in_max, long out_min, long out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-WebThingAdapter *adapter; // adapter could then be used as as device
+WebThingAdapter *adapter;  // adapter could then be used as as device
 ThingActionObject *action_generator(DynamicJsonDocument *);
+const char *baseTypes[] = {"WiFiBase", "MAC Address", nullptr};
 const char *lampTypes[] = {"OnOffSwitch", "Light", nullptr};
 const char *sensorTypes[] = {"Sensor", "Sensor", nullptr};
 const char *asyncProperties[] = {"asyncProperty", nullptr};
@@ -70,12 +74,13 @@ StaticJsonDocument<256> fadeInput;
 JsonObject fadeInputObj = fadeInput.to<JsonObject>();
 ThingAction fade("fade", "Fade", "Fade the lamp to a given level", "FadeAction", &fadeInputObj, action_generator);
 ThingEvent overheated("overheated", "The lamp has exceeded its safe operating temperature", NUMBER, "OverheatedEvent");
+ThingDevice wifiBase("wifi", "MAC Address", baseTypes);
 
-ThingDevice wifiBase("wifi","base","base address");
 ThingDevice lamp("ABC", "ABC", lampTypes);
 ThingDevice AHT10Device("AHT10", "AHT10", sensorTypes);
 ThingDevice textDisplay("asyncProperty", "Async Property Test", asyncProperties);
 
+ThingProperty base("wifi", "MAC of base WiFi", STRING, "MAC adress");
 ThingProperty lampOn("on", "Whether the lamp is turned on", BOOLEAN, "OnOffProperty");
 ThingProperty lampLevel("brightness", "The level of light from 0-100", INTEGER, "BrightnessProperty");
 ThingProperty AHT10TemperatureProperty("Temperature", "Temperature in C", NUMBER, "Centigrades");
@@ -83,13 +88,15 @@ ThingProperty AHT10HumidityProperty("Humidity", "Humidity (RH) %", NUMBER, "%");
 
 // Forward declaration
 void textDisplayTextChanged(ThingPropertyValue newVal);
-ThingProperty textDisplayText("text", "", STRING, nullptr, textDisplayTextChanged);
-//ThingProperty textDisplayToggle("toggle","",STRING,nullptr,textDisplayToggled);
-//ThingProperty textDisplayNumber("number","",STRING,nullptr,textDisplayNumbenewrChanged);
+ThingProperty textDisplayText("text", "", STRING, nullptr,
+                              textDisplayTextChanged);
+// ThingProperty
+// textDisplayToggle("toggle","",STRING,nullptr,textDisplayToggled);
+// ThingProperty
+// textDisplayNumber("number","",STRING,nullptr,textDisplayNumbenewrChanged);
 String message = "message";
 String lastMessage = message;
-void textDisplayTextChanged(ThingPropertyValue newVal)
-{
+void textDisplayTextChanged(ThingPropertyValue newVal) {
     String x = *newVal.string;
     ESP_LOGI(TAG, "text=>%s\n", x.c_str());
 }
@@ -100,144 +107,100 @@ const unsigned char redPin = 12;
 const unsigned char greenPin = 13;
 const unsigned char bluePin = 14;
 //---------------GPIO33 pulled low and connected to button to pull high to 3.3v
-#define BUTTON_WAKEUP_BITMASK 0x200000000 // 2^33 in hex / GPIO33
+#define BUTTON_WAKEUP_BITMASK 0x200000000  // 2^33 in hex / GPIO33
 //------------------------------------------------------------------
 
 //------------Adafruit Lib -----------------------------------------
 // the AHT must be connected to Bords SDA->GPIO21 and SCL ->GPIO22
 //  to detect the sensor board
 Adafruit_AHTX0 aht;
-//------------------------------------------------------------------
-LedControl lc = LedControl(pinDIN, pinCLK, pinCS, 1);
-//------------------------------------------------------------------
 WiFiClient client;
 MySQL_Connection mySqlConn(&client);
-
-const int baseid = 0;
-String baseKey = ""; // this is the Wifi Mac strip of :
-std::map<int, string> deviceMap;
-//char sqlStmt[] = "CALL IOT.insertDevice('%s','%s','%s');";
 char sQuery[300];
-
 String sqlStmt = "Call Iot.insertDevice('%s','%s','%s');";
-void registerDevice(ThingDevice *d, ThingProperty *p)
-{
-    sprintf(sQuery, sqlStmt.c_str(), d->id.c_str(), p->id.c_str(), p->description.c_str());
-    ESP_LOGI(TAG, "ready to sql = %s\n \t %s\n", sqlStmt, sQuery);
-
-    if (mySqlConn.connect(mySqlIP, sqlPort, sqlUser, sqlPassword))
-    {
-        // ESP_LOGI(TAG, "Connected in registerDevice for query \n\t delaying for 500\n->%s \n", sQuery);
-        delay(200);
-        MySQL_Cursor *curm = new MySQL_Cursor(&mySqlConn);
-        curm->execute(sQuery);
-        char *x = curm->getLastScalar();
-        p->propetyDbId = atoi(x);
-        ESP_LOGI(TAG, "Query=%s\n\t rv=%s\n\tpropertyDbId=%i \n", sQuery, x, p->propetyDbId);
-        curm->close();
-        delete curm;
-        mySqlConn.close();
-    }
-    else
-
-        ESP_LOGE(TAG, "Sql connect failed\n");
+//------------------------------------------------------------------
+LedControl lc = LedControl(pinDIN, pinCLK, pinCS, 1);
+//----------------Init MAX7219--------------------------------------
+void initMAX7219() {
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, HIGH);
+    Serial.begin(115200);
+    ESP_LOGI(TAG, "Connecting to %s \n", ssid);
+    //--- RTC wakeup on gpio 33 (wired to button)
+    esp_sleep_enable_ext1_wakeup(BUTTON_WAKEUP_BITMASK,
+                                 ESP_EXT1_WAKEUP_ANY_HIGH);
+    //------------------Initialize MAX7219-------------
+    ESP_LOGI(TAG, "Initializing MAX7219..\n");
+    lc.shutdown(0, false);
+    lc.setIntensity(0, 10);
+    lc.clearDisplay(0);
+    lc.printF(0, (char *)"%0.2f");
 }
-void setup(void)
-{
-    uint8_t macAddr[6] = {0};
-    esp_err_t ret = ESP_OK;
-    // setup pins init MAX7219
-    {
-
-        pinMode(ledPin, OUTPUT);
-        digitalWrite(ledPin, HIGH);
-        Serial.begin(115200);
-        ESP_LOGI(TAG, "Connecting to %s \n", ssid);
-        //--- RTC wakeup on gpio 33 (wired to button)
-        esp_sleep_enable_ext1_wakeup(BUTTON_WAKEUP_BITMASK, ESP_EXT1_WAKEUP_ANY_HIGH);
-        //------------------Initialize MAX7219-------------
-        ESP_LOGI(TAG, "Initializing MAX7219..\n");
-        lc.shutdown(0, false);
-        lc.setIntensity(0, 10);
-        lc.clearDisplay(0);
-        lc.printF(0, (char *)"%0.2f");
+//--------------------initWifi()------------------------------------
+void initWifi() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    bool blink = false;
+    while (WiFi.status() != WL_CONNECTED) {
+        printf(".");
+        digitalWrite(ledPin, blink ? LOW : HIGH);  // active low led
+        digitalWrite(ledPin, blink);
+        delay(80);
     }
-    //-----------Detect AHT ----------------------------
-    if (!aht.begin())
-    {
+}
+//----------------initAHT()-----------------------------------------
+void initAHT() {
+    if (!aht.begin()) {
         ESP_LOGI(TAG, "Could not find AHT ! \n");
-    }
-    else
+    } else
         ESP_LOGI(TAG, "AHT10 or AHT20 found \n");
-    //---------connect Wifi(STA); blink while connecting--------------
-    {
-        WiFi.mode(WIFI_STA);
-        WiFi.begin(ssid, password);
-        bool blink=false;
-        while (WiFi.status() != WL_CONNECTED)
-        {
-            printf(".");
-            digitalWrite(ledPin, blink ? LOW : HIGH); // active low led
-            digitalWrite(ledPin, blink);
-            delay(80);
-        }
-        baseKey = WiFi.macAddress();
-        baseKey.replace(":", "");
-        ESP_LOGI(TAG, "Connected to-> %s : localIP=%s: mac=%s \n", ssid, (char *)WiFi.localIP().toString().c_str(), baseKey.c_str());
-        ret = esp_efuse_mac_get_default(macAddr);
-    }
-    // connect MySQL on given SQLIP (in password.h)
-    {
-        if (mySqlConn.connect(mySqlIP, sqlPort, sqlUser, sqlPassword))
-        {
-            ESP_LOGI(TAG, "Connected to SQL On ->%s \n", (char *)mySqlIP.toString().c_str());
-            // exit test/wakeup (press button(on gpio33) to wakeup)
-            digitalWrite(ledPin, LOW); // turn off active low led
-        }
-        else
-        {
-            ESP_LOGI(TAG, "Oh.. could not connect to SQL %s \n ", (char *)mySqlIP.toString().c_str());
-            ESP_LOGI(TAG, "Placing this station to Deep Sleep!. Press the Button(GPIO33) to wakeup\n");
-            esp_deep_sleep_start();
-        };
-        // now get the registered device list;
+}
+//----------------initSQL()-----------------------------------------
+void initSQL() {
+    if (mySqlConn.connect(mySqlIP, sqlPort, sqlUser, sqlPassword)) {
+        ESP_LOGI(TAG, "Connected to SQL On ->%s \n", (char *)mySqlIP.toString().c_str());
 
-        // digitalWrite(ledPin, HIGH); // turn off active low led
-    }
-    // Init Mozilla thing and begin the adapter
+        digitalWrite(ledPin, LOW);  // turn off active low led
+    } else {
+        ESP_LOGI(TAG, "Oh.. could not connect to SQL %s \n ", (char *)mySqlIP.toString().c_str());
+        ESP_LOGI(TAG, "Placing this station to Deep Sleep!. Press the Button(GPIO33) to wakeup\n");
+        esp_deep_sleep_start();
+    };
+}
+//----------------initAdapterAndAddDevices()------------------------
+void initAdapterAndAddDevices() {
+    String x = WiFi.macAddress();
+    x.replace(":", "");
+    adapter = new WebThingAdapter(x, WiFi.localIP());
+    // devices and the properties
     {
-
-        String x = WiFi.macAddress();
-        x.replace(":", "");
-        adapter = new WebThingAdapter(x, WiFi.localIP());
-        adapter->addDevice(&wifiBase);
-        {
-            lamp.description = "A web conneced lamp";
-            lamp.title = "On/Off";
-
-            lampLevel.title = "Brightness";
-            lampLevel.minimum = 0;
-            lampLevel.maximum = 100;
-            lampLevel.unit = "%";
-            lamp.addProperty(&lampLevel);
-        }
+        // base.setValue(x.c_str());
+        printf("x.cstr()=%s\n", x.c_str());
+        wifiBase.id = x.c_str();
+        base.id = x.c_str();
+        printf("base desicrption=%s\n", base.description.c_str());
+        wifiBase.addProperty(&base);
+        lamp.description = "A web conneced lamp";
+        lamp.title = "On/Off";
+        lampLevel.title = "Brightness";
+        lampLevel.minimum = 0;
+        lampLevel.maximum = 100;
+        lampLevel.unit = "%";
+        lamp.addProperty(&lampLevel);
         // create fade Input object and json
-        {
-            fadeInputObj["type"] = "object";
-            JsonObject fadeInputProperties = fadeInputObj.createNestedObject("properties");
+        fadeInputObj["type"] = "object";
+        JsonObject fadeInputProperties = fadeInputObj.createNestedObject("properties");
 
-            JsonObject brightnessInput = fadeInputProperties.createNestedObject("brightness");
-            brightnessInput["type"] = "integer";
-            brightnessInput["minimum"] = 0;
-            brightnessInput["maximum"] = 100;
-            brightnessInput["unit"] = "percent";
+        JsonObject brightnessInput = fadeInputProperties.createNestedObject("brightness");
+        brightnessInput["type"] = "integer";
+        brightnessInput["minimum"] = 0;
+        brightnessInput["maximum"] = 100;
+        brightnessInput["unit"] = "percent";
 
-            JsonObject durationInput = fadeInputProperties.createNestedObject("duration");
-            durationInput["type"] = "integer";
-            durationInput["minimum"] = 1;
-            durationInput["unit"] = "milliseconds";
-        }
-        // add device(lamp) actions and events and add the Device to adapter then begin the adapter
+        JsonObject durationInput = fadeInputProperties.createNestedObject("duration");
+        durationInput["type"] = "integer";
+        durationInput["minimum"] = 1;
+        durationInput["unit"] = "milliseconds";
 
         lamp.addAction(&fade);
         overheated.unit = "degree C";
@@ -250,70 +213,89 @@ void setup(void)
         AHT10Device.addProperty(&AHT10HumidityProperty);
 
         textDisplay.addProperty(&textDisplayText);
-
-        adapter->addDevice(&lamp);
-        adapter->addDevice(&AHT10Device);
-        adapter->addDevice(&textDisplay);
-        adapter->begin();
     }
-    ESP_LOGE(TAG, "Adapter initialized \n");
+    adapter->addDevice(&wifiBase);
+    adapter->addDevice(&lamp);
+    adapter->addDevice(&AHT10Device);
+    adapter->addDevice(&textDisplay);
+    adapter->begin();
+    ESP_LOGI(TAG, "Webthing Adapter initialized with 5 devices\n");
+}
+//------------------------------------------------------------------
+void registerDevice(ThingDevice *d, ThingProperty *p) {
+    sprintf(sQuery, sqlStmt.c_str(), d->id.c_str(), p->id.c_str(),
+            p->description.c_str());
+    ESP_LOGI(TAG, "ready to sql = %s\n \t %s\n", sqlStmt.c_str(), sQuery);
 
+    if (mySqlConn.connect(mySqlIP, sqlPort, sqlUser, sqlPassword)) {
+        // ESP_LOGI(TAG, "Connected in registerDevice for query \n\t delaying
+        // for 500\n->%s \n", sQuery);
+        delay(200);
+        MySQL_Cursor *curm = new MySQL_Cursor(&mySqlConn);
+        curm->execute(sQuery);
+        char *x = curm->getLastScalar();
+        p->propetyDbId = atoi(x);
+        ESP_LOGI(TAG, "Query=%s\n\t rv=%s\n\tpropertyDbId=%i \n", sQuery, x,
+                 p->propetyDbId);
+        curm->close();
+        delete curm;
+        mySqlConn.close();
+    } else
+
+        ESP_LOGE(TAG, "Sql connect failed\n");
+}
+//----registerDevices() in the SQL Serve from the adapter-----------
+void registerDevices() {
     ThingDevice *d = adapter->getFirstDevice();
-    while (d)
-    {
+    while (d) {
         ThingProperty *p = d->firstProperty;
-        while (p)
-        {
+        while (p) {
             registerDevice(d, p);
             p = (ThingProperty *)p->next;
         }
         d = d->next;
     }
-    // prepare sql for events inserts
-    //sqlStmt ="CALL IOT.insertEvent('%s','%s');";
 }
-sensors_event_t humidity, temperature;
+//------------------------------------------------------------------
+void setup(void) {
+    uint8_t macAddr[6] = {0};
+    esp_err_t ret = ESP_OK;
+    initMAX7219();  // (1) --- init MAX7219
+    initAHT();      // (2)----- init AHT------------------------------
+    initWifi();     // (3)------init WiFi-----------------------------
+    ESP_LOGI(TAG, "Connected to-> %s : localIP=%s \n", ssid, (char *)WiFi.localIP().toString().c_str());
+    initSQL();                   // (4) --- init SQL-------------------------------
+    initAdapterAndAddDevices();  // (5) -- intiAdapterAndAddDevices()--------------
+    registerDevices();           // (6) -- registerDevices() in the adapter--------
+}
+
 static int i = 0;
 
-ThingPropertyValue toPvalueNumber(double n)
-{
+ThingPropertyValue toPvalueNumber(double n) {
     ThingPropertyValue pv;
     pv.number = n;
     return pv;
 }
-
-void registerValueEvent(int dbid, String val)
-{ //make sure teh sqlStatment is set properly to "Call IOT.InsertEvent('%i','%s');";
-
+void registerValueEvent(int dbid, String val) {  // make sure teh sqlStatment is set properly to
     sprintf(sQuery, sqlStmt.c_str(), dbid, val);
-
     ESP_LOGD(TAG, "Sql STatement = %s \n\t sQyert=%s\n", sqlStmt, sQuery);
 }
-void readAHT10()
-{
-    char *fs = "hisssa";
-    aht.getEvent(&humidity, &temperature);
-    //   gcvt(humidity.relative_humidity,5,fs);
-
-    // registerValueEvent(AHT10HumidityProperty.propetyDbId,std::to_string(humidity.relative_humidity))
-    //   registerValueEvent(AHT10HumidityProperty.propetyDbId,String(fs));
+void readAHT10() {
+    sensors_event_t humidity, temperature;
+    aht.getEvent(&humidity, &temperature);  //   gcvt(humidity.relative_humidity,5,fs);
     AHT10HumidityProperty.setValue(toPvalueNumber(humidity.relative_humidity));
     AHT10TemperatureProperty.setValue(toPvalueNumber(temperature.temperature));
-
     ESP_LOGI(TAG, "%05d Humidity=%.2lf%% dbid=%i : Tempurature=%.2lf dbid=%i \n", ++i, humidity.relative_humidity, AHT10HumidityProperty.propetyDbId, temperature.temperature, AHT10TemperatureProperty.propetyDbId);
 }
-void loop(void)
-{
-
+void loop(void) {
     digitalWrite(23, HIGH);
 
     readAHT10();
-    adapter->update(); // pushit to the iot gateway
+    adapter->update();  // pushit to the iot gateway
 
     delay(pushInterval);
 }
-void do_fade(const JsonVariant &input)
-{
+void do_fade(const JsonVariant &input) {
     JsonObject inputObj = input.as<JsonObject>();
     long long int duration = inputObj["duration"];
     long long int brightness = inputObj["brightness"];
@@ -334,8 +316,7 @@ void do_fade(const JsonVariant &input)
     ESP_LOGI(TAG, " Queu event(overheated) %2.2f\n", val.number);
     lamp.queueEventObject(ev);
 }
-ThingActionObject *action_generator(DynamicJsonDocument *input)
-{
+ThingActionObject *action_generator(DynamicJsonDocument *input) {
     String output;
     serializeJson(*input, output);
 
